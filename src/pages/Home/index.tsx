a12c8e92c2720e111ed95808a9aa73be1876fdc0
Govinda <govinda777@gmail.com>
2025-01-01 21:04:32 -0300

    up

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import Hero from '../../components/Hero';
 import Solutions from './Solutions';
+import IaDoEmpreendedor from './IaDoEmpreendedor';
 
 const Home: React.FC = () => {
   const heroImage = new URL('/public/home/hero.png', import.meta.url).href;
   const solutionsImage = new URL('/public/home/solutions.png', import.meta.url).href;
+  const iaLogo = new URL('/public/home/logo-ia-do-empreendedor.png', import.meta.url).href;
 
   return (
     <>
@@ -25,6 +27,15 @@ const Home: React.FC = () => {
         description="Na Xperience Consultoria Empresarial, oferecemos soluções práticas e imediatas para pequenos e empreendedores, combinando a experiência humana com a inovação da Inteligência Artificial. Somos especialistas em ajudar pequenos empreendedores a otimizar seus negócios de forma simples, eficiente e de impacto imediato."
         linkText="Conheça todas as soluções"
         solutionsImageSrc={solutionsImage}
+      />
+      <IaDoEmpreendedor
+        logoSrc={iaLogo}
+        title="IA do Empreendedor"
+        subtitle="chatbot Xperience"
+        description="Nossa nova ferramenta, IA do Empreendedor, permite que você faça uma avaliação rápida e personalizada do seu comércio e descubra oportunidades de melhoria em minutos."
+        highlightPart1="TUDO DE FORMA"
+        highlightPart2="GRATUITA!"
+        buttonText="Explore a IA"
       />
     </>
   );
